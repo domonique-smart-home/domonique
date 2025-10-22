f8595e545d8fabaa5ba9bc9b4d29ad4ab9cae8e7
Domonique Smart Home <128100307+domonique-smart-home@users.noreply.github.com>
2025-10-22 22:37:05 +0200

    Update customstart.sh

--- domoticz/customstart.sh
+++ domoticz/customstart.sh
@@ -2,18 +2,21 @@
 # Generate Serial if not exist
 #
 if [ ! -f /opt/domoticz/userdata/config/.domoniqueserial ]
-	then
-		echo "dmn"`date +%y`"r20"`date +%m%d`s$RANDOM > /opt/domoticz/userdata/config/.domoniqueserial
+        then
+                echo "dmn"`date +%y`"r20"`date +%m%d`$RANDOM > /opt/domoticz/userdata/config/.domoniqueserial
 fi
+cloudid=$(head -n 1 /opt/domoticz/userdata/config/.domoniqueserial | tr '[:lower:]' '[:upper:]')
 
 # Update packages to install
 apt-get update
 
 # Required apt packages for implemetations:
-# 	- alexa-remote-control: jq oathtool
+#       - alexa-remote-control: jq oathtool
 #
 apt-get install -y jq oathtool
 
 # Adding custom menu from ./config/templates
 #
 cp -v /opt/domoticz/userdata/config/templates/* /opt/domoticz/www/templates/
+
+sed -i "s|<button class=\"btn\">.*</button>|<button class=\"btn\">$cloudid</button>|" "/opt/domoticz/www/templates/domonique cloud.html"
